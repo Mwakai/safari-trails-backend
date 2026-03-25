77b0b8a41cd06242c762c67a99aaff42d632cc20
Mwakai <mwakaimwambala@gmail.com>
2026-03-25 16:14:47 +0300

    chore: roles and faker

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -5,6 +5,7 @@ use App\Enums\UserStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -18,7 +19,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('slug')->unique();
             $table->text('description')->nullable();
-            $table->json('permissions')->default('[]');
+            $table->json('permissions')->default(DB::raw("('[]')"));
             $table->boolean('is_system')->default(false);
             $table->timestamps();
         });
