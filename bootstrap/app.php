ebb4f00f7eec71e86ce636f16c4dd10d16959de5
Mwakai <mwakaimwambala@gmail.com>
2026-04-01 04:58:11 +0300

    feat: auth

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,8 @@ use App\Http\Middleware\CheckPermission;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Auth\AuthenticationException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,6 +19,10 @@ return Application::configure(basePath: dirname(__DIR__))
             'permission' => CheckPermission::class,
         ]);
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
+    ->withExceptions(function (Exceptions $exceptions) {
+        $exceptions->render(function (AuthenticationException $e, Request $request) {
+            return response()->json([
+                'message' => "Unauthenticated.",
+            ], 401);
+        });
     })->create();
